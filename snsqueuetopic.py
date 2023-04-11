c74a2cb69a034ebe4914bccdf7abe0cacc6eee8e
manoharponugoti27 <mxp60190@ucmo.edu>
2023-04-12 02:56:12 +0530

    message

---
+++ snsqueuetopic.py
@@ -0,0 +1,17 @@
+import boto3
+
+# create SNS client
+sns = boto3.client('sns')
+
+# create SNS topic
+response = sns.create_topic(Name='my-topic')
+
+# get topic ARN
+topic_arn = response['TopicArn']
+
+# add email as subscriber
+sns.subscribe(
+    TopicArn=topic_arn,
+    Protocol='email',
+    Endpoint='example@example.com'
+)
